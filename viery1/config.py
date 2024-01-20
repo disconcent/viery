bfcc9e873e5de5007af0a164f03f9041ee5808c3
disconcent <89609518+disconcent@users.noreply.github.com>
2024-01-20 11:47:38 +0700

    Update config.py

--- viery1/config.py
+++ viery1/config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5879606695:AAGSt-NPeLM2N1DOiUZnyHuLgT6T_VZ08K8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6364797106:AAEWwXuk8zCmC72Cma4YWTY9icUPGXHUod4")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "11381402"))
